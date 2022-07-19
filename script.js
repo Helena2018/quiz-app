dcd081a7b63ff8d87d00c6740d4d2c6151227054
Helena2018 <helena2020liu@gmail.com>
2022-07-19 14:21:39 +1200

    debug select data unidentified

--- script.js
+++ script.js
@@ -34,12 +34,13 @@ const quizData =[
 ];
 
 const quiz = document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.anwser')
+const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
 
 let currentQuiz = 0
 let score = 0
@@ -47,18 +48,35 @@ let score = 0
 loadQuiz()
 
 function loadQuiz() {
+
   deselectAnswers()
+
   const currentQuizData = quizData[currentQuiz]
-  console.log(currentQuizData)
 
   questionEl.innerHTML = currentQuizData.question
   a_text.innerHTML = currentQuizData.a
   b_text.innerHTML = currentQuizData.b
   c_text.innerHTML = currentQuizData.c
   d_text.innerHTML = currentQuizData.d
+
 }
 
 function deselectAnswers() {
   answerEls.forEach(answerEl => answerEl.checked = false)
 }
 
+function getSelected() {
+  let answer
+
+  answerEls.forEach(answerEl => {
+    if(answerEl.checked) {
+      answer = answerEl.id
+    }
+  })
+  return answer
+}
+
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected()
+
+})
